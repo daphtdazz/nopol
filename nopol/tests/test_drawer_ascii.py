06174b120c2632fc27842c2d4d96da5f1b0dd8b4
david <david.park@the7bridges.com>
2019-12-27 21:18:50 +0000

    parametrize asicc drawer test and add more complex test

--- nopol/tests/test_drawer_ascii.py
+++ nopol/tests/test_drawer_ascii.py
@@ -1,3 +1,6 @@
+import pytest
+from pytest import param as prm
+
 from nopol.drawers.ascii import ASCIIDrawer
 from nopol.node import Node
 
@@ -12,24 +15,13 @@ def test_ascii_drawer_single_node():
 """
 
 
-def test_ascii_drawer():
-    n1 = Node()
-    n2 = Node()
-    n3 = Node()
-    n4 = Node()
-    n5 = Node()
-
-    n1.add_forward_node(n2)
-    n1.add_forward_node(n3)
-    n2.add_forward_node(n4)
-    n2.add_forward_node(n5)
-
-    drwr = ASCIIDrawer(n1)
-
-    drwr.build()
-    res = drwr.draw()
-
-    assert res == r"""* Node 1
+@pytest.mark.parametrize('edges,result', [
+    prm([
+        ('Node 1', 'Node 2'),
+        ('Node 1', 'Node 3'),
+        ('Node 2', 'Node 4'),
+        ('Node 2', 'Node 5'),
+    ], r"""* Node 1
 |\
 | \
 |  * Node 2
@@ -37,7 +29,54 @@ def test_ascii_drawer():
 |  | \
 |  |  * Node 4
 |  * Node 5
-* Node 3"""
+* Node 3""", id='simple-fan'),
+    prm([
+        ('Node 1', 'Node 2'),
+        ('Node 1', 'Node 3'),
+        ('Node 1', 'Node 6'),
+        ('Node 2', 'Node 4'),
+        ('Node 4', 'Node 7'),
+        ('Node 2', 'Node 5'),
+        ('Node 5', 'Node 8'),
+        ('Node 5', 'Node 9'),
+    ], r"""* Node 1
+|\
+| \
+|  * Node 2
+|  |\
+|  | \
+|  |  * Node 4
+|  |  * Node 7
+|  * Node 5
+|  |\
+|  | \
+|  |  * Node 8
+|  * Node 9
+|\
+| \
+|  * Node 3
+* Node 6""", id='various-splits-only'),
+])
+def test_ascii_drawer(edges, result):
+
+    nodes = {}
+    base_node = None
+    for edge in edges:
+        from_node_name, to_node_name = edge
+        for name in (from_node_name, to_node_name):
+            if name not in nodes:
+                nodes[name] = Node(name)
+
+        nodes[from_node_name].add_forward_node(nodes[to_node_name])
+        if base_node is None:
+            base_node = nodes[from_node_name]
+
+    drwr = ASCIIDrawer(base_node)
+
+    drwr.build()
+    res = drwr.draw()
+
+    assert res == result
 """
    *
   /|\
